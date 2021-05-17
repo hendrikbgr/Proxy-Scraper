c14b9d1eebb172cda67c6e4b5af8ec098311cde7
hendrikbgr <hendrik@whalar.com>
2021-05-17 16:32:27 +0200

    0.0.2

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 from requests_html import HTMLSession
-import url_config
 import requests
 import re
 import csv
 import json
+import sys
+import urllib.request, socket
+from threading import Thread
+import asyncio
+from sys import argv
+import aiohttp
 
+
+version = "0.0.2"
 proxy_list = []
 proxy_count = 0
 
-for url in url_config.urls:
+try:
+    with open('urls.txt', 'r') as file:
+        urls = [ line.rstrip() for line in file.readlines()]
+except FileNotFoundError:
+    raise Exception('urls.txt not found.')
+
+url_count = len(urls)
+
+print('--------------------------------------')
+print('- Easy Proxy Scraper & Checker {} -'.format(version))
+print('- Found {} URLS to Scrape            -'.format(url_count))
+print('--------------------------------------')
+print()
+print()
+check_yn = input('Check proxies after? [y/n] ')
+
+for url in urls:
     session = HTMLSession()
     print()
     print('Crawling Proxies from: ', url)
@@ -52,3 +75,44 @@ for url in url_config.urls:
 print('TOTAL NUMBER OF PROXIES CRAWLED: ', proxy_count)
 with open('proxies.txt', mode='wt', encoding='utf-8') as myfile:
     myfile.write('\n'.join(proxy_list))
+
+if check_yn == 'y':
+    
+    for proxy in proxy_list:
+        proxy_type = "http"
+        test_url = "http://www.google.com"
+        timeout_sec = 4
+
+        # read the list of proxy IPs in proxyList from the first Argument given
+        proxyList = proxy_list
+
+
+        async def is_bad_proxy(ipport):
+            try:
+                session = aiohttp.ClientSession()
+                resp = await session.get(test_url, proxy=ipport, timeout=timeout_sec)
+                if not resp.headers["Via"]:
+                    raise "Error"
+                print("Working:", ipport)
+                file = open("checked.txt", "a")
+                file.write("{}\n".format(proxy))
+                file.close()
+            except:
+                print("Not Working:", ipport)
+            session.close()
+
+        tasks = []
+
+        loop = asyncio.get_event_loop()
+
+        for item in proxyList:
+            tasks.append(asyncio.ensure_future(is_bad_proxy("http://" + item)))
+
+        print("Starting... \n")
+        loop.run_until_complete(asyncio.wait(tasks))
+        print("\n...Finished")
+        loop.close()
+        #print('{} - Success'.format(proxy))
+
+else:
+    pass

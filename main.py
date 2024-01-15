096b4cb28f63063abe604f8f8b6f9d18d56647f9
Hendrik Bagger <hendrik@whalar.com>
2024-01-15 11:17:17 +0100

    Remove duplicate and unused proxy URLs

--- main.py
+++ main.py
@@ -5,35 +5,20 @@
 from requests_html import HTMLSession
 import requests
 import re
-import csv
-import json
-import sys
-import urllib.request, socket
-from threading import Thread
-import asyncio
-import sys
-import aiohttp
-import datetime
-import urllib.request, urllib.parse, urllib.error
-import http.cookiejar
 import threading
 import queue
 import itertools
-
-# Etc
 import time
 from colorama import Fore, Back, Style
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from urllib.parse import urljoin
+
 
 version = "1.0"
-proxy_list = []
-proxy_count = 0
-bad_proxy_count = 0
-good_proxy_count = 0
-checked_count = 0
 
 try:
     with open('urls.txt', 'r') as file:
-        urls = [ line.rstrip() for line in file.readlines()]
+        urls = [line.rstrip() for line in file.readlines()]
 except FileNotFoundError:
     raise Exception('urls.txt not found.')
 
@@ -46,238 +31,80 @@ print('--------------------------------------')
 print()
 print()
 
-for url in urls:
-    session = HTMLSession()
-    print()
-    print('Crawling Proxies from: ', url)
+# Scrape Proxies
+proxy_set = set()
+session = HTMLSession()
 
-    # Get Proxy Content
-    
-    print()
+
+def scrape_proxies(url):
     try:
         r = session.get(url)
-        links = r.html.absolute_links
-        links = str(links)
-        links = links.replace('{', '')
-        links = links.replace('}', '')
-        links = links.split(',')
-        for link in links:
-            print('Link Scraped: ', link)
-            print()
+        proxies_found = re.findall(r'\d+\.\d+\.\d+\.\d+\:\d+', r.text)
+        print(f"Found {len(proxies_found)} proxies on {url}")
+        proxy_set.update(proxies_found)
+    except Exception as e:
+        print(f"Error scraping {url}: {e}")
 
-        # Get Proxies
-        page = r.html.find('html', first=True)
-        proxies_found = re.findall('\d+\.\d+\.\d+\.\d+\:\d+', page.html)
-        proxy_list = proxy_list + proxies_found
-        for proxy in proxy_list:
-            print('Proxy found: ', proxy)
-            proxy_count += 1
+for url in urls:
+    print('Crawling Proxies from: ', url)
+    scrape_proxies(url)  # Scrape the initial URL
 
-        session.close()
+    try:
+        # Scrape all URLs found on the page
+        all_links = session.get(url).html.absolute_links
+        for link in all_links:
+            scrape_proxies(link)
+    except Exception as e:
+        print(f"Error scraping links from {url}: {e}")
 
-        for link in links:
-
-            print('Crawling Proxies from: ', link)
-
-            try:
-                r = session.get(link)
-                page = r.html.find('html', first=True)
-                proxies_found = re.findall('\d+\.\d+\.\d+\.\d+\:\d+', page.html)
-                proxy_list = proxy_list + proxies_found
-                for proxy in proxy_list:
-                    proxy_count += 1
-                    print('Proxy found: ', proxy)
-                session.close()
-            except:
-                pass
-    except:
-        pass
-    
-
-print()
-print('TOTAL NUMBER OF PROXIES CRAWLED: ', len(proxy_list))
+session.close()
 
 # Save Proxies to File
-proxy_list = list(dict.fromkeys(proxy_list))
 with open('proxies.txt', mode='wt', encoding='utf-8') as myfile:
-    myfile.write('\n'.join(proxy_list))
+    myfile.write('\n'.join(proxy_set))
 
-print('TOTAL NUMBER AFTER DUPE REMOVE: ', len(proxy_list))
-print()
-print()
-print()
+print('TOTAL NUMBER OF PROXIES CRAWLED: ', len(proxy_set))
 
 # Check Proxies
+def check_proxy(proxy):
+    """
+    Check if a proxy is working by attempting to access a known website through it.
+    Returns True if the proxy is good, False otherwise.
+    """
+    test_url = 'http://www.google.com'
+    proxies = {
+        'http': proxy,
+        'https': proxy
+    }
 
+    try:
+        response = requests.get(test_url, proxies=proxies, timeout=5)
+        if response.status_code == 200:
+            print(f"Proxy {proxy} is working.")
+            return True
+    except requests.RequestException as e:
+        print(f"Proxy {proxy} failed: {e}")
 
-print('--------------------------------------')
-print('- Easy Proxy Scraper & Checker {} -'.format(version))
-print('- Found {} Proxies to Check         -'.format(len(proxy_list)))
-print('--------------------------------------')
-print()
-print()
+    return False
 
+def main():
+    with open('proxies.txt', 'r') as file:
+        proxies = file.read().splitlines()
 
+    total_proxies = len(proxies)
+    print(f"Total proxies to check: {total_proxies}")
 
-# Global variables
-in_filename  = 'proxies.txt'
-#in_directory = './'
-out_filename = 'checked.txt'
-test_url = 'http://www.google.com/humans.txt'
-thread_number = 100
-timeout_value = 10
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        future_to_proxy = {executor.submit(check_proxy, proxy): proxy for proxy in proxies}
+        for future in as_completed(future_to_proxy):
+            proxy = future_to_proxy[future]
+            try:
+                is_good = future.result()
+                if is_good:
+                    with open('checked.txt', 'a') as out_file:
+                        out_file.write(proxy + '\n')
+            except Exception as e:
+                print(f"Error checking proxy {proxy}: {e}")
 
-ok_msg = Fore.GREEN + "OK!  " + Fore.RESET
-fail_msg = Fore.RED + "FAIL " + Fore.RESET
-
-# Stats
-good_proxy_num = itertools.count()
-start_time = time.time()
-end_time   = time.time()
-
-# Safe print()
-mylock = threading.Lock()
-def sprint(*a, **b):
-    with mylock:
-        print(*a, **b)
-
-
-#
-# Printer
-#
-class PrintThread(threading.Thread):
-    def __init__(self, queue, filename):
-        threading.Thread.__init__(self)
-        self.queue = queue
-        self.output = open(filename, 'a')
-        self.shutdown = False
-
-    def write(self, line):
-        print(line, file=self.output)
-
-    def run(self):
-        while not self.shutdown:
-            lines = self.queue.get()
-            self.write(lines)
-            self.queue.task_done()
-
-    def terminate(self):
-        self.output.close()
-        self.shutdown = True
-
-
-
-#
-# Processor
-#
-class ProcessThread(threading.Thread):
-    def __init__(self, id, task_queue, out_queue):
-        threading.Thread.__init__(self)
-        self.task_queue = task_queue
-        self.out_queue  = out_queue
-        self.id = id
-
-    # ...
-    def run(self):
-        while True:
-            task   = self.task_queue.get()
-            result = self.process(task)
-
-            if result is not None:
-                self.out_queue.put(result)
-                next(good_proxy_num)
-
-            self.task_queue.task_done()
-
-
-    # Do the processing job here
-    def process(self, task):
-        proxy = task
-        log_msg = str("Thread #%3d.  Trying HTTP proxy %21s \t\t" % (self.id, proxy))
-
-        cj =  http.cookiejar.CookieJar()
-        opener = urllib.request.build_opener(
-                    urllib.request.HTTPCookieProcessor(cj),
-                    urllib.request.HTTPRedirectHandler(),
-                    urllib.request.ProxyHandler({ 'http' : proxy })
-        )
-
-        try:
-            t1 = time.time()
-            response = opener.open(test_url, timeout=timeout_value).read()
-            t2 = time.time()
-        except Exception as e:
-            log_msg += "%s (%s)" % (fail_msg, str(e))
-            sprint(log_msg)
-            return None
-
-        log_msg += ok_msg + " Response time: %d, length=%s" % ( int((t2-t1)*1000), str(len(response)) )
-        sprint(log_msg)
-        return proxy
-
-    def terminate(self):
-        None
-        #print("Thread #%d is down..." % (self.id))
-
-#
-# Main starts here
-#
-# Init some stuff
-input_queue  = queue.Queue()
-result_queue = queue.Queue()
-
-
-# Spawn worker threads
-workers = []
-for i in range(0, thread_number):
-    t = ProcessThread(i, input_queue, result_queue)
-    t.setDaemon(True)
-    t.start()
-    workers.append(t)
-
-# Spawn printer thread to print
-f_printer = PrintThread(result_queue, out_filename)
-f_printer.setDaemon(True)
-f_printer.start()
-
-# Add some stuff to the input queue
-start_time = time.time()
-
-import os
-
-for proxy in proxy_list:
-    input_queue.put(proxy)
-
-total_proxy_num = len(proxy_list)
-print("got %d proxies to check" % total_proxy_num)
-
-if total_proxy_num == 0:
-    exit()
-
-# Wait for queue to get empty
-input_queue.join()
-result_queue.join()
-
-
-#while (not input_queue.empty()):
-#    time.sleep(1)
-
-
-# Shutdown
-f_printer.terminate()
-
-for worker in workers:
-    worker.terminate()
-
-
-# Print some info
-good_proxy_num = float(next(good_proxy_num))
-print("In: %d. Good: %d, that's %.2f%%" % (total_proxy_num, good_proxy_num, 100.0 * good_proxy_num/total_proxy_num))
-
-end_time = time.time()
-print("Time elapsed: %.1f seconds." % (end_time - start_time))
-print("Bye-bye!")
-
-
-final_result = "Total Proxies Crawled: {} | Total Good Proxies: {} | Download: https://cutt.ly/Lm8Vjq8 | #freeproxies #proxylist #freeproxylist #proxies #crackingtools #proxy #freestuff".format(total_proxy_num, good_proxy_num)
-print(final_result)
+if __name__ == "__main__":
+    main()
